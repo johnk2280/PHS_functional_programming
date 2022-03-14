8315651b857122c0f7206429b3b589f8463a94d1
johnk2280 <johnk2280@gmail.com>
2022-03-14 12:44:03 +0500

    [upd] solution

--- task_6.fs
+++ task_6.fs
@@ -6,11 +6,16 @@ let rec pow = function
 
 
 // 17.2
-let isIthChar(s: string, n, c) = s.[n] = c
+let isIthChar(s: string, n, c) = function
+ | (s, n, c) when n > (String.length s) - 1 -> false
+ | (s, n, c) when n < 0 -> false
+ | (s, n, c) -> s.[n] = c
 
 
 // 17.3
 let rec occFromIth = function
  | (s, n, c) when n > (String.length s) - 1 -> 0
- | (s, n, c) when not (isIthChar(s, n, c)) -> 0 + occFromIth(s, n + 1, c)
- | (s, n, c) when isIthChar(s, n, c) -> 1 + occFromIth(s, n + 1, c)
+ | (s, n, c) when n < 0 -> 0
+ | (s, n, c) when s.[n] <> c -> 0 + occFromIth(s, n + 1, c)
+ | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, n + 1, c)
+
