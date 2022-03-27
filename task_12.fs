ff0df870954cb8bfc2039e9c10eb8bb0d32984cd
johnk2280 <johnk2280@gmail.com>
2022-03-28 00:05:25 +0500

    [upd] evenn

--- task_12.fs
+++ task_12.fs
@@ -12,7 +12,8 @@ let rec dnto = function
 
 // 34.3
 let rec evenn = function
- | n when n < 0 -> []
+ | n when n <= 0 -> []
  | n when n % 2 = 0 -> evenn(n - 2) @ [n]
  | n when n % 2 = 1 -> evenn(n - 3) @ [n - 1]
 
+evenn 5
