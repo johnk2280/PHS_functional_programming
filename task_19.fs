c5f93d7a0de9a6345cf50032ccccf2399433f56a
johnk2280 <johnk2280@gmail.com>
2022-04-27 21:43:43 +0500

    [upd] task 19

--- task_19.fs
+++ task_19.fs
@@ -27,3 +27,19 @@ let rec bigList n k =
 let a = bigList 230000 id
 
 // List.length a
+
+// let rec fib1 n n1 n2 = 
+//     match n with
+//     | 0 -> n2
+//     | 1 -> n1
+//     | n -> fib1 (n - 1) (n1 + n2) n1
+
+// let rec fib2 n c = 
+//     match n with
+//     | 0 -> c 0
+//     | 1 -> c 1
+//     | n -> fib2 (n - 2) (fun a -> fib2 (n - 1) (fun b -> c (a + b)))
+
+// let rec bigList n k =
+//   if n = 0 then k []
+//   else bigList (n - 1) (fun res -> k(1::res))
